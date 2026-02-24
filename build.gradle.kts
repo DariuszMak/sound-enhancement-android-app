9837fbf5a6d4f02213cf7c72b600de468429a6e9
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-02-24 12:41:09 +0100

    Revert "add dependencies to gradle"
    This reverts commit adbe1ddf97b8818cd2c3c9b729a4a54c8e052faf.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply true
-}
-
-android {
-    namespace = "com.example.soundenchancement"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.soundenchancement"
-        minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.11.0")
-
-    testImplementation("junit:junit:4.13.2")
-
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("androidx.test:core:1.5.0")
+    alias(libs.plugins.android.application) apply false
 }
